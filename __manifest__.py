4f2c460eea5a3f7ebce09847e302c5b33a328c6e
aneravanzosc <aneravanzosc@gmail.com>
2026-06-11 09:34:44 +0200

    [DOC] manifest: update website field to OCA repo URL

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Tema y contenido a medida para avanzosc.es sobre Odoo 14.",
     "version": "18.0.1.0.0",
     "category": "Website/Theme",
-    "website": "https://avanzosc.es",
+    "website": "https://github.com/avanzosc/odoo-addons",
     "author": "Avanzosc S.L.",
     "license": "AGPL-3",
     "depends": [
